7cd0fcd815bf649da35bee74279734ca35e97b65
Itzel Contreras <itselfcg@gmail.com>
2021-06-25 11:16:14 -0700

    Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'home-app',
-  templateUrl: 'home.component.html'
-  //styleUrls: ['./app.component.scss']
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss','../app.component.scss']
 })
 export class HomeComponent {}
