18644bd55035b492973317838d263f81c0e78706
Itzel Contreras <itselfcg@gmail.com>
2021-06-25 15:38:26 -0700

    Projects page

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { slideInOutAnimation,fadeAnimation } from '../_animations/index';
 
 @Component({
   selector: 'work-app',
   templateUrl: 'work.component.html',
-  styleUrls: ['work.component.scss']
+  styleUrls: ['work.component.scss','../app.component.scss'],
+  animations: [fadeAnimation]
+
+
 })
-export class WorkComponent {}
+export class WorkComponent {
+  animation = true;
+
+
+}
