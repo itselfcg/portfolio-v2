1f5a295470d090768879ce6eac8568e71614e61e
Itzel Contreras <itselfcg@gmail.com>
2021-06-30 12:22:09 -0700

    Models were updated.

---
+++ src/app/_models/case-study.model.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  id:string,
+  name: string;
+  title: string;
+  description: string;
+  vision: string;
+  challenge: string;
+  introduction: string;
+  users: Object[];
+  insights: Object[];
+  pictures: Object[];
+}
