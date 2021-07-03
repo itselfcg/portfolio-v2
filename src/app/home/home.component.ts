2b731e980a326efaf66b0511e83b43bf9ac9ed07
Itzel Contreras <itselfcg@gmail.com>
2021-07-03 14:32:15 -0700

    Style fixes, padding top and bottom to content tag

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from '../_animations/index';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'home-app',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.scss','../app.component.scss'],
+  styleUrls: ['../app.component.scss','home.component.scss'],
   animations: [fadeAnimation]
 })
 export class HomeComponent {
   animation = true;
+  constructor(public translate: TranslateService) {
+    if (localStorage.getItem('language')) {
+      translate.setDefaultLang(localStorage.getItem('language')!);
+      translate.use(localStorage.getItem('language')!);
+    } else {
+      console.log(2343);
+      translate.setDefaultLang('en');
+      translate.use('en');
+      localStorage.setItem('language', 'en');
+    }
+  }
 
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    localStorage.setItem('language', language);
 
+  }
 }
