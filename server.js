16537a905e463d849e91b0c9163f321f29a405cd
Itzel Contreras <itselfcg@gmail.com>
2021-07-07 15:41:53 -0700

    config

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'dist','portfolio-v2')));
+app.use(express.static(__dirname + '/dist/portfolio-v2'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname,'dist','app_name','portfolio-v2'));
+  res.sendFile(path.join(__dirname + '/dist/portfolio-v2/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8000);
