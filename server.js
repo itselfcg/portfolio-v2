8f3142449829f344f8860bfcb09125e070eda5d1
Itzel Contreras <itselfcg@gmail.com>
2021-07-07 16:22:47 -0700

    Configs

--- server.js
+++ server.js
@@ -10,5 +10,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/portfolio-v2/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
