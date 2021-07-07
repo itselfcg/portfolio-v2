40bbc1a97c82e85a66208c4f9c36a8111cfc700f
Itzel Contreras <itselfcg@gmail.com>
2021-07-07 15:00:54 -0700

    Feature section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about-app',
   templateUrl: 'about.component.html',
-  styleUrls: ['../app.component.scss']
+  styleUrls: ['../app.component.scss','about.component.scss']
 })
 export class AboutComponent {
   animation = true;
