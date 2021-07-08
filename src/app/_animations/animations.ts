f92b60a0c347e0def2e6134ff66e45c2ac88b8ba
Itzel Contreras <itselfcg@gmail.com>
2021-07-08 11:21:00 -0700

    Transitions were made optional to first time entering a section bug.

--- src/app/_animations/animations.ts
+++ src/app/_animations/animations.ts
@@ -23,24 +23,22 @@ export const slideInAnimation = trigger('routeAnimations', [
     animate("500ms  ease-in",style({ opacity: 1,  })),
   ]),
   transition('*=>*', [
-    query(':enter, :leave', style({ position: 'fixed', width: '100%' })),
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {optional: true}),
     group([
       query(
         ':enter',
         [
           style({ transform: 'translateY(100%)' }),
           animate('.3s ease-out', style({ transform: 'translateY(0%)' })),
-        ]
+        ], {optional: true}
       ),
       query(
         ':leave',
         [
           style({ transform: 'translateY(0%)' }),
           animate('.3s ease-out', style({ transform: 'translateY(-100%)' })),
-        ]
+        ], {optional: true}
       ),
-    ]),
-    query(':leave', animateChild()),
-    query(':enter', animateChild()),
+    ])
   ])
 ]);
