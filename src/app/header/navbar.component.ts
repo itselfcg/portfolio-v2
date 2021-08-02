5e5509e24637478422c01c19174361e76f88592d
Itzel Contreras <itselfcg@gmail.com>
2021-08-02 15:19:52 -0700

    smart devices navbar upgraded

--- src/app/header/navbar.component.ts
+++ src/app/header/navbar.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { NavbarService } from '../_services/navbar.service';
 
 @Component({
   selector: 'navbar-app',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor( public nav: NavbarService ) {}
+  isHome = false;
+  isHomeExpanded=false;
+  constructor(private router: Router, public nav: NavbarService) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log(event.urlAfterRedirects);
 
+        if (event.urlAfterRedirects !== '/') {
+          this.isHome = false;
+        } else {
+          this.isHome = true;
+        }
+      }
+    });
+  }
   ngOnInit() {
     this.nav.show();
   }
 
 }
-
