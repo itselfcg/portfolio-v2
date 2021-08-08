410dfb00d9cc1f5532eaaaaccb838a8275236efe
Itzel Contreras <itselfcg@gmail.com>
2021-08-08 15:14:30 -0700

    Translation service endpoint was changed

--- src/app/_services/translation.service.ts
+++ src/app/_services/translation.service.ts
@@ -4,12 +4,15 @@ import { Observable } from 'rxjs';
 import { TranslateLoader } from '@ngx-translate/core';
 import { map } from 'rxjs/operators';
 
+import { environment } from 'src/environments/environment';
+const API_URL=environment.apiUrl;
+
 @Injectable({ providedIn: 'root' })
 export class TranslationService implements TranslateLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string): Observable<any> {
-    return this.http.get(`http://localhost:3000/api/translations/${lang}`).pipe(
+    return this.http.get(`${API_URL}/translations/${lang}`).pipe(
       map((response: any) => {
         return response.result;
       })
